0ab294cc89e21cb90229d85089ab5d936757484a
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2018-09-22 00:21:28 -0400

    include subtration to the calculateNumbers function and test

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,9 @@ export const calculateNumbers = (num1, num2, op) => {
   // addition
   if (op === '+') {
     total = num1 + num2
+    // subtration
+  } else if (op === '-') {
+    total = num1 - num2
   }
   return total
 }
--- src/utils.test.js
+++ src/utils.test.js
@@ -5,11 +5,20 @@ const testNumbers = {
     op: '+',
     add1: 1,
     add2: 2
+  },
+  sub: {
+    op: '-',
+    sub1: 7,
+    sub2: 3
   }
 }
 describe('calculateNumbers will calculate the numbers that are passed into it', () => {
   let add = testNumbers.add
+  let sub = testNumbers.sub
   test('calculateNumbers will add the two numbers passed into it and return the total', () => {
     expect(calculateNumbers(add.add1, add.add2, add.op)).toEqual(3)
+  })
+  test('calculateNumbers will subtract the two numbers passed into it and return the total', () => {
+    expect(calculateNumbers(sub.sub1, sub.sub2, sub.op)).toEqual(4)
   })
 })

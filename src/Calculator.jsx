bce7af7bd2360c15e68a04317474071de63e1517
Ted Weiss <tedweiss@Teds-MacBook-Pro.local>
2018-09-22 23:04:18 -0400

    display digits on page

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
+import { digits } from './data'
+
 export default class Calculator extends Component {
   render () {
-    return <div className='Calculator' />
+    return (
+      <div className='Calculator'>
+        {digits.map(digit => {
+          return <div>{digit.name}</div>
+        })}
+      </div>
+    )
   }
 }
